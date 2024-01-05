7d51b6f69f22fc57227080c9cb096df02139e61b
hieutm1501 <hieuhop2003@gmail.com>
2024-01-05 22:48:38 +0700

    update

--- src/pages/welcom/Welcom.jsx
+++ src/pages/welcom/Welcom.jsx
@@ -1,46 +1,28 @@
-import React from 'react'
-import './index.css'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import './index.css';
+import { Link } from 'react-router-dom';
 
-const Welcom = () => {
+const Welcome = () => {
+
+
     return (
-        <div>
-            <div class="birthdayCard">
-                <div class="cardFront">
-                    <div class="front-text">
-                        <h3 class="happy">HAPPY</h3>
-                        <h2 class="bday">BIRTHDAY</h2>
-                        <h3 class="toyou">Gấu Trúc!</h3>
-                    </div>
-                    <div class="wrap-deco">
-                        <div class="decorations"></div>
-                        <div class="decorationsTwo"></div>
-                    </div>
-                    <div class="wrap-decoTwo">
-                        <div class="decorations"></div>
-                        <div class="decorationsThree"></div>
-                    </div>
-                    <div class="plate">
-                        <div class="cake"></div>
-                        <div class="flame"></div>
+        <div className='mt-[30px]'>
+            <div class="card">
+                <div class="back"></div>
+                <div class="front">
+                    <div class="imgset">
+                        <img width="100%" src="https://1.bp.blogspot.com/-Mgj9-rbs65E/XfMoPSD5gtI/AAAAAAAAURk/NBokE2gSS2cTSJ2em5lZ5hJDuTtRN7UVwCLcBGAsYHQ/s1600/2713997.png" />
                     </div>
                 </div>
-
-                <div class="cardInside">
-                    <div class="inside-text">
-                        <h3 class="happy">HAPPY</h3>
-                        <h2 class="bday">BIRTHDAY</h2>
-                        <h3 class="toyou">Gấu Trúc</h3>
-                    </div>
-                    <div class="wishes">
-                        <p>Xin Chào gấu trúc,</p>
-                        <p>Happy birthday!! Chúc Lan có một sinh nhật thật vui vẻ và hạnh phúc.Tuổi mới chúc Lan có thật nhiều niềm vui và thuận gió hoà mưa công việc của mình nhé.Một lần nữa chúc Lan sinh nhật vui vẻ nha.</p>
-                        <p class="name">Hiếu</p>
-                    </div>
+                <div class="text-container">
+                    <p id="head">Happy Birthday!</p>
+                    <h5>From Ashish to You</h5>
+                    <p>I hope your special day will bring you lots of happiness, love, and fun. You deserve them a lot. Enjoy!</p>
+                    <p>Hope your day goes great!</p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Welcom
+export default Welcome;
